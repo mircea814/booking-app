4b92ae1d4a4d5fc940bbfd8c2a408b25a69d9591
Felix Ticleanu <felix.ticleanu@creato-design.com>
2025-05-22 23:57:01 +0200

    edit logIn

--- frontend/src/LogIn/LogIn.js
+++ frontend/src/LogIn/LogIn.js
@@ -3,15 +3,14 @@ import './LogIn.css';
 
 function LogIn() {
     return (
-        <div className="login-container">
-            <h1>Log1In</h1>
-            <form> 
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Log In</button>
-            </form>
-
-        </div>
+        <div className='login-container'>
+        <h1>Log In</h1>
+       <form className='login-form'>
+        <input type="text" placeholder="Username" className='login-input' required />
+        <input type="password" placeholder="Password" className='login-input'  re/>
+        <button type="submit" className='login-button'>Log In</button>
+       </form>
+      </div>
     );
 }
 
